e639e3ccd3fe0a611fb511b317215e3e72824335
Dc_Yuki <dc_yuki@qq.com>
2021-02-27 23:09:23 +0800

    update gobang
    - 优化五子棋代码

--- yumemi/plugins/gobang/index.js
+++ yumemi/plugins/gobang/index.js
@@ -1,3 +1,7 @@
+const board = [];
+const piece = ['●', '○'];
+const alphabet = 'ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ';
+const numbers = ['　', '①', '②', '③', '④', '⑤', '⑥', '⑦', '⑧', '⑨', '⑩', '⑪', '⑫', '⑬', '⑭', '⑮', '⑯', '⑰', '⑱', '⑲', '⑳'];
 const delta = [
   // 横向
   [[0, -1], [0, 1]],
@@ -11,42 +15,32 @@ const delta = [
 const allboard = {};
 
 module.exports = (messageData, option) => {
-  let boardSize = null;
-  let [boardLeft, boardRight] = [null, null];
-  const board = [];
-  const alphabet = 'ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ';
-  const numbers = ['　', '①', '②', '③', '④', '⑤', '⑥', '⑦', '⑧', '⑨', '⑩', '⑪', '⑫', '⑬', '⑭', '⑮', '⑯', '⑰', '⑱', '⑲', '⑳'];
-
-  const start = (size = 10) => {
-    if (allboard[messageData.group_id] !== undefined) {
-      bot.sendGroupMsg(messageData.group_id, `当前群聊对局尚未结束，请勿重复开启`);
-    } else {
+  const start = (size = 8) => {
+    if (allboard[messageData.group_id] !== undefined) bot.sendGroupMsg(messageData.group_id, `当前群聊对局尚未结束，请勿重复开启`);
+    else {
       boardSize = size;
-      for (let [i, j] = [0, 0]; i < boardSize; j++) {
-        if (j === 0) board[i] = [], j += 3;
+
+      for (let [i, j] = [0, 0]; i <= boardSize; j++) {
+        if (j === 0) board[i] = [];
         switch (i) {
           case 0:
-            board[i].push('┬');
-            boardLeft = '┌';
-            boardRight = '┐';
+            board[i].push(j === 0 ? '┌' : (j === boardSize ? '┐' : '┬'));
             break;
-          case boardSize - 1:
-            board[i].push('┴');
-            boardLeft = '└';
-            boardRight = '┘';
+          case boardSize:
+            board[i].push(j === 0 ? '└' : (j === boardSize ? '┘' : '┴'));
             break;
+
           default:
-            board[i].push('┼');
-            boardLeft = '├';
-            boardRight = '┤';
+            board[i].push(j === 0 ? '├' : (j === boardSize ? '┤' : '┼'));
             break;
         }
 
-        if (j === boardSize) board[i].push(boardRight), board[i].unshift(boardLeft), board[i].unshift(alphabet[i]), i++, j = -1;
+        if (j === boardSize) board[i].unshift(alphabet[i]), i++, j = -1;
       }
 
-      numbers.length = boardSize + 1;
+      numbers.length = boardSize + 2;
       board.unshift(numbers);
+
       allboard[messageData.group_id] = {
         board: board,
         black: messageData.user_id,
@@ -59,90 +53,69 @@ module.exports = (messageData, option) => {
   }
 
   const moves = () => {
-    if (allboard[messageData.group_id] === undefined) {
-      bot.sendGroupMsg(messageData.group_id, `当前群聊未开启对局`);
-      return;
-    }
-    if (allboard[messageData.group_id].white === null && messageData.user_id !== allboard[messageData.group_id].black) {
+    let reg = {
+      x: /[a-zA-Z]/g,
+      y: /[0-9]/g,
+    };
+    const x = messageData.raw_message.match(reg.x).join().toUpperCase().charCodeAt() - 64;
+    const y = parseInt(messageData.raw_message.match(reg.y).join(''));
+    const chessman = [[x, y]];
+
+    if (allboard[messageData.group_id] === undefined) return bot.sendGroupMsg(messageData.group_id, `当前群聊未开启对局`);
+
+    if (allboard[messageData.group_id].white === null && allboard[messageData.group_id].black !== messageData.user_id) {
       allboard[messageData.group_id].white = messageData.user_id;
       bot.sendGroupMsg(messageData.group_id, `[CQ:at,qq=${messageData.user_id}] 加入当前对局`);
     }
-    // const position = [];
-    let reg = /[a-zA-Z]/g;
-    const x = messageData.raw_message.match(reg).join().toUpperCase().charCodeAt() - 64;
-    reg = /[0-9]/g;
-    const y = parseInt(messageData.raw_message.match(reg).join(''));
 
-    if (allboard[messageData.group_id].state === 'black' && messageData.user_id === allboard[messageData.group_id].black) {
-      if ( allboard[messageData.group_id].board[x][y] === '●' || allboard[messageData.group_id].board[x][y] === '○') {
-        bot.sendGroupMsg(messageData.group_id, `[CQ:at,qq=${messageData.user_id}] 当前位置已经有棋子啦，换个位置下吧`);
+    if (messageData.user_id === allboard[messageData.group_id].white || messageData.user_id === allboard[messageData.group_id].black) {
+      if (allboard[messageData.group_id].board[x][y] === undefined) return bot.sendGroupMsg(messageData.group_id, `[CQ:at,qq=${messageData.user_id}] 越界了，亲 (╯▔皿▔)╯`);
+      if (allboard[messageData.group_id].board[x][y] === piece[0] || allboard[messageData.group_id].board[x][y] === piece[1]) return bot.sendGroupMsg(messageData.group_id, `[CQ:at,qq=${messageData.user_id}] 当前位置已经有棋子啦，换个位置下吧`);
+      // allboard[messageData.group_id].state = 
+      // allboard[messageData.group_id].black === messageData.user_id && allboard[messageData.group_id].state?
+      // 'white'?'black'
+      if (allboard[messageData.group_id].black === messageData.user_id && allboard[messageData.group_id].state === 'black') {
+        allboard[messageData.group_id].state = 'white';
+        allboard[messageData.group_id].board[x][y] = piece[0];
+      } else if (allboard[messageData.group_id].white === messageData.user_id && allboard[messageData.group_id].state === 'white') {
+        allboard[messageData.group_id].state = 'black';
+        allboard[messageData.group_id].board[x][y] = piece[1];
+      } else {
+        bot.sendGroupMsg(messageData.group_id, `[CQ:at,qq=${messageData.user_id}] 还没到你的回合呢，你急啥 (lll￢ω￢)`);
         return;
       }
-      allboard[messageData.group_id].state = 'white';
-      allboard[messageData.group_id].board[x][y] = '●';
-    } else if (allboard[messageData.group_id].state === 'white' && messageData.user_id === allboard[messageData.group_id].white) {
-      if ( allboard[messageData.group_id].board[x][y] === '●' || allboard[messageData.group_id].board[x][y] === '○') {
-        bot.sendGroupMsg(messageData.group_id, `[CQ:at,qq=${messageData.user_id}] 当前位置已经有棋子啦，换个位置下吧`);
-        return;
-      }
-      allboard[messageData.group_id].state = 'black';
-      allboard[messageData.group_id].board[x][y] = '○';
-    } else if (messageData.user_id === allboard[messageData.group_id].black || messageData.user_id === allboard[messageData.group_id].white) {
-      bot.sendGroupMsg(messageData.group_id, `[CQ:at,qq=${messageData.user_id}] 还没到你的回合呢，你急啥`);
-      return;
+      bot.sendGroupMsg(messageData.group_id, allboard[messageData.group_id].board.join('\n'));
     } else {
-      bot.sendGroupMsg(messageData.group_id, `[CQ:at,qq=${messageData.user_id}] 你以后下象棋必被人指指点点！`);
+      bot.sendGroupMsg(messageData.group_id, `[CQ:at,qq=${messageData.user_id}] 你以后下象棋必被人指指点点！<( ￣^￣)(θ(θ☆( >_<`);
       return;
     }
-    // allboard[messageData.group_id].board[x][y] = messageData.user_id === allboard[messageData.group_id].black ?
-    //   '●' :
-    //   (
-    //     messageData.user_id === allboard[messageData.group_id].white ?
-    //       '○' :
-    //       bot.sendGroupMsg(messageData.group_id, `[CQ:at,qq=${messageData.user_id}] 你以后下象棋必被人指指点点！`)
-    //   );
-    // const [x, y] = position;
+
     for (const [left, right] of delta) {
-      let newX, newY, rightX, rightY = null;
+      let newX, newY = null;
       for (let [i, j] = [1, 1]; i < 5; i++) {
-        if (j === 1) {
-          newX = x + left[0] * i;
-          newY = y + left[1] * i;
-        } else {
-          newX = rightX + right[0] * i;
-          newY = rightY + right[1] * i;
-        }
-        // newX = x + (j === 1 ? left[0] : right[0]) * i;
-        // newY = y + (j === 1 ? left[1] : right[1]) * i;
-        console.log(`i: ${i}`);
-        if (i === 4 && allboard[messageData.group_id].board[newX][newY] === allboard[messageData.group_id].board[x][y]) {
-          bot.sendGroupMsg(messageData.group_id, allboard[messageData.group_id].board.join('\n'));
-          bot.sendGroupMsg(messageData.group_id, `check mate！恭喜 [CQ:at,qq=${messageData.user_id}] 获得本轮胜利~`);
+        if (chessman.length === 5) {
+          bot.sendGroupMsg(messageData.group_id, `check mate！恭喜 [CQ:at,qq=${messageData.user_id}] 获得本轮胜利~ ヾ(≧▽≦*)o`);
           delete allboard[messageData.group_id];
           return;
         }
-        console.log(newX, newY)
-        // 判断是否有邻子
-        if (allboard[messageData.group_id].board[newX][newY] !== allboard[messageData.group_id].board[x][y]) {
-          // 判断是否越界
-          if (newX < 1 || newY < 1 || newX > boardSize || newY > boardSize) {
-            console.log('越界');
+
+        newX = x + (j === 1 ? left[0] : right[0]) * i;
+        newY = y + (j === 1 ? left[1] : right[1]) * i;
+
+        if (board[newX][newY] === board[x][y]) {
+          chessman.push([newX, newY]);
+        } else {
+          if (j === 1) {
+            j++, i = 0;
           } else {
-            console.log(`${j === 1 ? '左侧' : '右侧'} 没邻子`);
+            j--;
+            chessman.length = 1;
+            break;
           }
-          rightX = x + left[0] * (i - 1);
-          rightY = y + left[1] * (i - 1);
-          if (j === 1) { j++; i = 0; } else { j--; break; }
         }
       }
     }
-
-    bot.sendGroupMsg(messageData.group_id, allboard[messageData.group_id].board.join('\n'));
   }
 
-  const over = () => {
-    delete allboard[messageData.group_id];
-    bot.sendGroupMsg(messageData.group_id, `已结束当前对局`);
-  }
   eval(`${option}()`);
 }
