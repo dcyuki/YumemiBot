6acd76279a0de55afbd0631408128d1f50616474
Dc_Yuki <dc_yuki@qq.com>
2021-05-05 20:52:37 +0800

    update battle

--- plugins/_web/router/api.js
+++ plugins/_web/router/api.js
@@ -1,13 +1,42 @@
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(`${__yumemi}/data/db/yumemi.db`);
 
 const api = new Router();
 api.use(bodyParser());
 
 api.post('/battle/:action', async ctx => {
   const { action } = ctx.params;
-
+  const { sql, params } = ctx.request.body;
   
+  db.serialize(() => {
+    switch (action) {
+      case 'get':
+        db.get(sql, params, (err, row) => {
+          !err ? resolve(row) : reject(err);
+        })
+        break;
+
+      case 'all':
+        db.all(sql, params, (err, rows) => {
+          !err ? resolve(rows) : reject(err);
+        })
+        break;
+        
+      case 'run':
+        db.run(sql, params, err => {
+          !err ? resolve() : reject(err);
+        })
+        break;
+
+      default:
+        ctx.status = 404;
+        break;
+    }
+
+    db.close();
+  });
 })
 
 api.get('/send/:target', async ctx => {
