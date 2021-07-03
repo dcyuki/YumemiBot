ae85ad9d76bbceae41bfac114515f8081af61f9d
Dc_Yuki <dc_yuki@qq.com>
2021-07-03 16:22:57 +0800

    update plugin
    - 重构 greet
    - 重构 chess
    - 更新 emoji

---
+++ plugins/chess/demo.js
@@ -0,0 +1,18 @@
+function listener(data) {
+  const { raw_message } = data;
+  const action = checkCommand('chess', raw_message);
+
+  action && eval(`${action}(data, this)`);
+}
+
+function activate(bot) {
+  bot.on("message.group", listener);
+}
+
+function deactivate(bot) {
+  bot.off("message.group", listener);
+}
+
+module.exports = {
+  activate, deactivate
+}
